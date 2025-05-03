3fe4337d391768145cfc344bb6dc04dcaad35344
Ashley Maher <ashley.maher@didymodesigns.com.au>
2025-05-03 12:33:50 +1000

    more refactoring

--- src/kilimanjaro_oncology/gui/app.py
+++ src/kilimanjaro_oncology/gui/app.py
@@ -1,29 +1,3 @@
-# app.py
-# import tkinter as tk
-# from tkinter import ttk
-#
-#
-# class Application(tk.Tk):
-#     def __init__(self):
-#         super().__init__()
-#
-#         self.title("My Tkinter App")
-#         self.geometry("800x600")
-#
-#         # Configure main window
-#         self.configure(padx=10, pady=10)
-#
-#         # Create main frame
-#         self.main_frame = ttk.Frame(self)
-#         self.main_frame.pack(expand=True, fill="both")
-#
-#         # Add your widgets here
-#         self.label = ttk.Label(self.main_frame, text="Hello, World!")
-#         self.label.pack(pady=20)
-#
-#     def run(self):
-#         self.mainloop()
-
 
 import tkinter as tk
 from tkinter import ttk
